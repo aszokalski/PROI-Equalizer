a9cda2e64374c42d120c6bef65d3c656247c10ca
kkrol1 <01178545@pw.edu.pl>
2023-06-04 21:19:26 +0200

    Added .h files for classes helpful in testing PluginProcessor and PluginEditor.

--- tests/test_EqualizerEditor.cpp
+++ tests/test_EqualizerEditor.cpp
@@ -1,3 +1,24 @@
 //
 // Created by kkrol on 31.05.2023.
 //
+#include <catch2/catch_test_macros.hpp>
+#include "../src/PluginProcessor/PluginProcessor.h"
+#include "./src/PluginEditor/PluginEditor.h"
+
+TEST_CASE("Test lowSlider value", "[lowSlider]")
+{
+    EqualizerProcessor processor;
+    EqualizerEditor editor(processor);
+    editor.lowSlider.setValue(0.5f);
+    REQUIRE(editor.lowSlider.getValue() == 0.5f);
+}
+
+TEST_CASE("Test lowSlider range"), "[lowSlider]")
+{
+    EqualizerProcessor processor;
+    EqualizerEditor editor(processor);
+    editor.lowSlider.setRange(0.0f, 1.0f, 0.01f);
+    REQUIRE(editor.lowSlider.getMinimum() == 0.0f);
+    REQUIRE(editor.lowSlider.getMaximum() == 1.0f);
+    REQUIRE(editor.lowSlider.getInterval() == 0.01f);
+}
--- tests/test_EqualizerProcessor.cpp
+++ tests/test_EqualizerProcessor.cpp
@@ -1,3 +1,7 @@
 //
 // Created by kkrol on 31.05.2023.
 //
+#include <catch2/catch_test_macros.hpp>
+#include "../src/PluginProcessor/PluginProcessor.h"
+#include "./src/PluginEditor/PluginEditor.h"
+
